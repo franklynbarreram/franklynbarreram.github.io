1e2890454a4a32e9ebc136055acb028eeda6a5db
franklyn.barrera <fbarrera@shokworks.io>
2020-03-18 22:43:26 -0400

    Updates

---
+++ precache-manifest.c8a152605221741cca4f47f14764a7a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69eb2a6af951eb13cb88",
+    "url": "/React/static/css/main.050eb6bd.chunk.css"
+  },
+  {
+    "revision": "69eb2a6af951eb13cb88",
+    "url": "/React/static/js/main.f3eb2e31.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "6a733133fe37a8d58266",
+    "url": "/React/static/js/2.a98b5cb2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "77d16138bace26c97f7a8d8f8bd2a156",
+    "url": "/React/index.html"
+  }
+];
